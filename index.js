30409aa2face512294d8f79f4664b90d2caa77db
Adam Rees <adam.g.rees@gmail.com>
2012-11-25 15:17:22 +0000

    missed index.js on tag

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-//TODO: CreateTarget in Grunt.js, build npm package, link into node_modules
-/*
-{ lo0: 
-   [ { address: '::1', family: 'IPv6', internal: true },
-     { address: 'fe80::1', family: 'IPv6', internal: true },
-     { address: '127.0.0.1', family: 'IPv4', internal: true } ],
-  en1: 
-   [ { address: 'fe80::cabc:c8ff:feef:f996', family: 'IPv6',
-       internal: false },
-     { address: '10.0.1.123', family: 'IPv4', internal: false } ],
-  vmnet1: [ { address: '10.99.99.254', family: 'IPv4', internal: false } ],
-  vmnet8: [ { address: '10.88.88.1', family: 'IPv4', internal: false } ],
-  ppp0: [ { address: '10.2.0.231', family: 'IPv4', internal: false } ] }
- *
- *
- */
-
 var os = module.exports;
 var _networkInterfaces;
-
 var regIPv4 = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))/;
 
-os.networkInterfaces = function(){
-	return _networkInterfaces;
-}
-
-
-
-chrome.socket.getNetworkList( onNetworkList );
-
-function onNetworkList( networkInterfaces ) {
+var onNetworkList = function( networkInterfaces ) {
 	_networkInterfaces = {};
 	networkInterfaces.forEach( function( nInterface ) {
 		addInterface( nInterface.name, nInterface.address );
 	});
 };
 
-function addInterface( name, address ) {
+var addInterface = function( name, address ) {
 	if ( !( name in _networkInterfaces ) ) {
 		_networkInterfaces[ name ] = [];	
 	}
@@ -47,13 +21,20 @@ function addInterface( name, address ) {
 
 };
 
-function getAddressFamily( address ) {
+var getAddressFamily = function( address ) {
 	if ( address.match( regIPv4 ) ) {
 		return 'IPv4';
 	}else{
 		return 'IPv6';
 	}
 };
+
+
+os.networkInterfaces = function(){
+	return _networkInterfaces;
+}
+// This is async in Chrome so it's called on init
+chrome.socket.getNetworkList( onNetworkList );
 
 
 ;['tmpDir'
